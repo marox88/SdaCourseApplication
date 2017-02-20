093504f9c8b9ba549b470d7bc669085a01606b2b
MaRoX <marek.kuzniecow@gmail.com>
2017-02-20 21:00:17 +0100

    third

--- app/src/main/java/com/example/rent/sdacourseapplication/drawing/DrawingActivity.java
+++ app/src/main/java/com/example/rent/sdacourseapplication/drawing/DrawingActivity.java
@@ -3,6 +3,7 @@ package com.example.rent.sdacourseapplication.drawing;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -11,11 +12,13 @@ import com.example.rent.sdacourseapplication.R;
 
 public class DrawingActivity extends AppCompatActivity {
 
+    private SimpleDrawingView simpleDrawingView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.drawing_activity_main);
-        final SimpleDrawingView simpleDrawingView = (SimpleDrawingView) findViewById(R.id.drawing_view);
+        simpleDrawingView = (SimpleDrawingView) findViewById(R.id.drawing_view);
         Button clearButton = (Button) findViewById(R.id.clear_button);
 
         clearButton.setOnClickListener(new View.OnClickListener() {
@@ -28,8 +31,20 @@ public class DrawingActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.clear_menu) {
+            simpleDrawingView.clear();
+        }
         return super.onOptionsItemSelected(item);
     }
+
+
+
 
 }
