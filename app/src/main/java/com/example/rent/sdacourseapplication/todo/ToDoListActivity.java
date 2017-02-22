99268877fbe1172e9f1f4e8fe27190a22f408b4f
MaRoX <marek.kuzniecow@gmail.com>
2017-02-22 20:49:39 +0100

    Todo Activity added

---
+++ app/src/main/java/com/example/rent/sdacourseapplication/todo/ToDoListActivity.java
@@ -0,0 +1,26 @@
+package com.example.rent.sdacourseapplication.todo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.rent.sdacourseapplication.R;
+import com.example.rent.sdacourseapplication.drawing.DrawingActivity;
+
+
+public class ToDoListActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.todo_activity);
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/rent/sdacourseapplication/todo/TodoListAdapter.java
@@ -0,0 +1,58 @@
+package com.example.rent.sdacourseapplication.todo;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.rent.sdacourseapplication.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by RENT on 2017-02-22.
+ */
+
+public class TodoListAdapter extends RecyclerView.Adapter<TodoListAdapter.MyViewHolder> {
+
+
+    List<String> items = new ArrayList<>();
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.list_item,parent,false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        holder.textView.setText(items.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public void addItem(String item) {
+        items.add(item);
+        notifyDataSetChanged();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView textView;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            textView = (TextView) itemView.findViewById(R.id.item_text);
+        }
+    }
+
+
+
+
+}
